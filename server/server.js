7a895dd17f10987f41e73a178016828752b2996d
cnagadya <ncritah@gmail.com>
2018-06-24 19:05:40 +0300

    Setting up the routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
+"use strict";
+
 const express = require("express");
 const mongoose = require("mongoose");
 
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
-
 const db = require("./config/keys").mongoURI;
-
 mongoose
   .connect(db)
   .then(() => console.log("Successfully connected to MongoDB"))
@@ -15,7 +19,11 @@ mongoose
     )
   );
 
+// App routes
 app.get("/", (req, res) => res.send("Hello Christine!"));
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 8001;
 
